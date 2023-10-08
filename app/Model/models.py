09d5fbf4596ffa8a42f05693ec6b3d9a1e1f04a9
WillRInternship <willwrae22@gmail.com>
2023-10-08 14:57:33 -0700

    adding comments

--- app/Model/models.py
+++ app/Model/models.py
@@ -1,10 +1,12 @@
 from app import db
 
+# Table of Many-to-Many relationship for Position and Experiences model
 positionExperiences = db.Table('positionExperiences',
     db.Column('position_id', db.Integer, db.ForeignKey('position.id')),
     db.Column('experience_id', db.Integer, db.ForeignKey('experience.id'))
 )
 
+# Table of Many-to-Many relationship for Position and Research Fields model
 positionFields = db.Table('positionFields',
     db.Column('position_id', db.Integer, db.ForeignKey('position.id')),
     db.Column('field_id', db.Integer, db.ForeignKey('field.id'))
@@ -17,6 +19,8 @@ class Position(db.Model):
     startDate = db.Column(db.String(30))
     endDate = db.Column(db.String(30))
     timeCommitment = db.Column(db.Integer())
+    # By defining the research fields and experiences as models in the database,
+    # users are able to add additional fields and experiences later if there isn't one already present they need.
     fields = db.relationship('models.Field',secondary=positionFields, primaryjoin=(positionFields.c.position_id == id), backref=db.backref('positionFields', lazy='dynamic'), lazy='dynamic')
     experiences = db.relationship('models.Experience',secondary=positionExperiences, primaryjoin=(positionExperiences.c.position_id == id), backref=db.backref('positionExperiences', lazy='dynamic'), lazy='dynamic')
     qualifications = db.Column(db.String(300))
